f91f6a0686d887eb0e6a0d2b85447a571f3bad07
Felix Abalia <faabalia@gmail.com>
2021-03-05 10:14:31 +0100

    Ruta y Vista detalle de usuario. Fake-backend con json-server. Crear ambiente desarrollo y producción

--- src/app/modules/user/user-list/user-list.component.ts
+++ src/app/modules/user/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ICardUser} from '@shared/components/cards/card-user/icard-user.metadata';
+import {USERS_DATA} from '@data/constants/users.const';
 
 @Component({
   selector: 'app-user-list',
@@ -7,58 +8,7 @@ import {ICardUser} from '@shared/components/cards/card-user/icard-user.metadata'
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements OnInit {
-  public users: ICardUser[] = [
-    {
-      id: 1,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Ivan Larios',
-      age: 27,
-      description: 'Desarrollador fullstack',
-      work: 'Project manager'
-    },
-    {
-      id: 2,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Martín Larios',
-      age: 27,
-      description: 'Diseñador',
-    },
-    {
-      id: 3,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Luis Larios',
-      age: 27,
-      description: 'Desarrollador back',
-    },
-    {
-      id: 4,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Héctor Larios',
-      age: 27,
-      description: 'Desarrollador front',
-    },
-    {
-      id: 5,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Martín Larios',
-      age: 27,
-      description: 'Diseñador',
-    },
-    {
-      id: 6,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Luis Larios',
-      age: 27,
-      description: 'Desarrollador back',
-    },
-    {
-      id: 7,
-      avatar: 'https://ideapod.com/wp-content/uploads/2017/08/person-1.jpg',
-      name: 'Héctor Larios',
-      age: 27,
-      description: 'Desarrollador front',
-    }
-  ];
+  public users: ICardUser[] = USERS_DATA;
 
   constructor() { }
 
